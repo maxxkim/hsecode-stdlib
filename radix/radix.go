92013ca80fb33814f04b4d7e932e20406904fc6c
maxxkim <maxxkim@ya.ru>
2020-10-07 16:26:34 +0300

    radix1

--- radix/radix.go
+++ radix/radix.go
@@ -1,42 +1,81 @@
 package radix
 
 import (
-	"bytes"
-	"encoding/binary"
+	"sort"
 )
 
-const digit = 4
-const maxbit = -1 << 31
+const (
+	MinSize      = 256
+	radix   uint = 8
+	bitSize uint = 64
+)
 
-func Sort(data []uint64) {
-	buf := bytes.NewBuffer(nil)
-	ds := make([][]byte, len(data))
-
-	for i, e := range data {
-		binary.Write(buf, binary.LittleEndian, e^maxbit)
-		b := make([]byte, digit)
-		buf.Read(b)
-		ds[i] = b
+func Sort(x []uint64) {
+	if len(x) < MinSize {
+		sort.Slice(x, func(i, j int) bool { return x[i] < x[j] })
+	} else {
+		buffer := make([]uint64, len(x))
+		SortBYOB(x, buffer)
 	}
-	countingSort := make([][][]byte, 256)
+}
 
-	for i := 0; i < digit; i++ {
-		for _, b := range ds {
-			countingSort[b[i]] = append(countingSort[b[i]], b)
-		}
-		j := 0
-		for k, bs := range countingSort {
-			copy(ds[j:], bs)
-			j += len(bs)
-			countingSort[k] = bs[:0]
-		}
+func SortBYOB(x, buffer []uint64) {
+	if len(x) > len(buffer) {
+		panic("Buffer so small")
+	}
+	if len(x) < 2 {
+		return
 	}
 
-	var w uint64
+	from := x
+	to := buffer[:len(x)]
+	var key uint8
 
-	for i, b := range ds {
-		buf.Write(b)
-		binary.Read(buf, binary.LittleEndian, &w)
-		data[i] = w ^ maxbit
+	for keyOffset := uint(0); keyOffset < bitSize; keyOffset += radix {
+		var offset [256]int
+		sorted := true
+		var prev uint64 = 0
+
+		for _, elem := range from {
+
+			key = uint8(elem >> keyOffset)
+			offset[key]++
+			if sorted {
+				sorted = elem >= prev
+				prev = elem
+			}
+		}
+
+		if sorted {
+			if (keyOffset/radix)%2 == 1 {
+				copy(to, from)
+			}
+			return
+		}
+		watermark := 0
+		if keyOffset == bitSize-radix {
+			for i := 128; i < len(offset); i++ {
+				count := offset[i]
+				offset[i] = watermark
+				watermark += count
+			}
+			for i := 0; i < 128; i++ {
+				count := offset[i]
+				offset[i] = watermark
+				watermark += count
+			}
+		} else {
+			for i, count := range offset {
+				offset[i] = watermark
+				watermark += count
+			}
+		}
+
+		for _, elem := range from {
+			key = uint8(elem >> keyOffset)
+			to[offset[key]] = elem
+		}
+
+		from, to = to, from
 	}
 }
